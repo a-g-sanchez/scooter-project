8facfd0a1abd00382042cb1add48e20e496b1ca1
a-g-sanchez <aarongsanchez@google.com>
2023-09-28 15:30:15 -0500

    Can register a user and check if they are over 18 or if they already exist as a user

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -13,7 +13,29 @@ class ScooterApp {
     this.registeredUsers = {}
   }
 
+  registerUser(username, password, age){
+    if(age<18){
+      console.log('Too young to register')
+      throw new Error('Too young to register')
+    }
+    
+    if(!this.registeredUsers[username] && age >= 18){
+    let newUser = new User(username, password, age)
+    this.registeredUsers[newUser.username] = newUser
+    console.log('user has been registered')
+    return newUser
+    } else if(this.registeredUsers[username] && age >=18){
+      console.log('Already Registerd')
+      throw new Error('Already Registered')
+    }
+  }
 }
 
+let theSA = new ScooterApp()
+
+let personOne = theSA.registerUser('Person One', '123Wurd', 18)
+let personTwo = theSA.registerUser('Person Two', '123wurd', 76)
+
+console.log(theSA)
 
 module.exports = ScooterApp
