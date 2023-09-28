2b2a0a666e760f437fb9be66c5f033a073e91195
a-g-sanchez <aarongsanchez@google.com>
2023-09-28 16:56:02 -0500

    A user can be created with necessary properties

--- src/User.js
+++ src/User.js
@@ -1,5 +1,15 @@
 class User {
   // User code here
+  constructor(username, password, age){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedIn = false
+  }
 }
+
+const aaron = new User('Aaron', '123wurd', 83)
+
+console.log(aaron)
 
 module.exports = User
